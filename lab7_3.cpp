880a156f6c5341d301d7d84f236d66ee46d235b9
theeramet0793 <theerametmetha@gmail.com>
2020-01-21 15:34:53 +0700

    commit-1

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,61 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+main(){
+
+   string status;
+   float age,height,asset;
+
+   cout<<"Input Age : ";
+   cin>>age;
+   if(age<=20){
+    cout<<"Height : ";
+    cin>>height;
+
+
+       if(height<160){
+       status="UNFRIENDS";
+       }else if(height<175)
+         {
+        status="FRIEND";
+        }
+        else {cout<<" Asset :  ";
+         cin>>asset;
+             if(asset>6900000){
+        status="MARRIED";
+    }
+    else
+    {
+        status="ONE-NIGHT-STAND";
+    }}
+
+       
+
+   }
+
+
+
+
+
+   else if(age<30)
+   { 
+       cout<<"Asset : ";
+       cin>>asset;
+       if(asset>1000000){
+           status="BEST FRIEND";
+       }
+       else
+       {
+           status="UNFRIEND";
+       }
+       
+   }
+   else
+   {
+       status="UNFRIEND";
+   }
+   
+   cout<<status;
+
+}
